22bc942e38cf119650ab3f858706b882cae37f8f
Amit Maraj <amit.maraj@gmail.com>
2020-09-06 11:14:27 -0400

    autocomplete user search.

--- src/components/Root/Home.js
+++ src/components/Root/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Link
@@ -6,6 +6,8 @@ import {
 
 import CentreContainer from '../Shared/CentreContainer'
 import Layout from '../Shared/Layout'
+
+import { AutoComplete } from 'antd';
 
 import config from '../../config'
 console.log(config)
@@ -21,34 +23,82 @@ const graphqlWithAuth = graphql.defaults({
 
 
 const Home = ({}) => {
+  const [value, setValue] = useState('');
+  const [options, setOptions] = useState([]);
+
+  const onSearch = async (searchText) => {
+    if (searchText.length >= 2){
+      const {search} = await graphqlWithAuth(searchUser(searchText));
+      setOptions(search.nodes.map(n => { return {value: `${n.name} | ${n.login}`} }))
+    } else {
+      setOptions([])
+    }
+    // setOptions([{ value: "some random string" }]);
+  };
+
+  const onSelect = (data) => {
+    console.log('onSelect', data);
+  };
+
+  const onChange = (data) => {
+    setValue(data);
+  };
 
 
   useEffect(() => {
-    (async () => {
-      const { repository } = await graphqlWithAuth(`
-      {
-        repository(owner: "maraja", name: "league-side") {
-          issues(last: 3) {
-            edges {
-              node {
-                title
-              }
-            }
-          }
-        }
-      }
-    `);
-    console.log(repository)
-    })()
+    // (async () => {
+    //   const {search} = await graphqlWithAuth(`
+    //   {
+    //     search(query: "maraja", type: USER, first: 10) {
+    //       nodes {
+    //         ... on User {
+    //           id
+    //           email
+    //           bio
+    //           name
+    //           login
+    //           url
+    //         }
+    //       }
+    //     }
+    //   }
+    // `);
+    // console.log(repository)
+    // })()
   }, [])
 
   return (
     <Layout>
     <CentreContainer>
       <h1>Welcome home!</h1>
+      <AutoComplete
+        options={options}
+        style={{ width: 200 }}
+        onSelect={onSelect}
+        onSearch={onSearch}
+        placeholder="input here"
+      />
     </CentreContainer>
     </Layout>
   )
 };
 
 export default Home;
+
+
+const searchUser = username => `
+{
+  search(query: "${username}", type: USER, first: 10) {
+    nodes {
+      ... on User {
+        id
+        email
+        bio
+        name
+        login
+        url
+      }
+    }
+  }
+}
+`
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export default {
-    github_token: '9f7e7fc484c30575c16d48970d79d10f9ad15c00'
+    github_token: 'b9291024136d5771068c81ff02fb32485b632dbf'
 }
